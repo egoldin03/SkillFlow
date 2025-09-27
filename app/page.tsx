3f09148dd7ffe06dfeac7256ccaafd5cdf6dddab
Emanuel Goldin <egoldin03@gmail.com>
2025-09-27 14:36:43 -0400

    Got rid of boilerplate code and tutorial code for supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-import { DeployButton } from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
 import { AuthButton } from "@/components/auth-button";
-import { Hero } from "@/components/hero";
 import { ThemeSwitcher } from "@/components/theme-switcher";
-import { ConnectSupabaseSteps } from "@/components/tutorial/connect-supabase-steps";
-import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/lib/utils";
+import { PushupIcon } from "@/components/chart/PushupIcon";
+import { PushProgressIcon } from "@/components/chart/PushProgressIcon";
+import { StatsIcon } from "@/components/chart/StatsIcon";
 import Link from "next/link";
 
 export default function Home() {
@@ -15,21 +12,11 @@ export default function Home() {
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Next.js Supabase Starter</Link>
-              <div className="flex items-center gap-2">
-                <DeployButton />
-              </div>
+              <Link href={"/"}>SkillFlow</Link>
             </div>
-            {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
+            <AuthButton />
           </div>
         </nav>
-        <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
-          <Hero />
-          <main className="flex-1 flex flex-col gap-6 px-4">
-            <h2 className="font-medium text-xl mb-4">Next steps</h2>
-            {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-          </main>
-        </div>
 
         <div className="font-sans flex items-center justify-center min-h-screen p-8">
           <main className="flex flex-col items-center gap-8">
@@ -48,17 +35,6 @@ export default function Home() {
         </div>
 
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-          <p>
-            Powered by{" "}
-            <a
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-              target="_blank"
-              className="font-bold hover:underline"
-              rel="noreferrer"
-            >
-              Supabase
-            </a>
-          </p>
           <ThemeSwitcher />
         </footer>
       </div>
